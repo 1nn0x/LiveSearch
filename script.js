695c82b43d91f8f9e696a91f7166ea0a63d9892b
Gwedoh <anthony.d.morano@gmail.com>
2016-12-14 11:45:05 -0600

    Start implementing search

---
+++ script.js
@@ -0,0 +1,18 @@
+$("#search").keyup(function ()
+{
+    $.getJSON('data.json', function (data)
+    {
+        var output = '<ul class = "searchresults">';
+        $.each(data, function (key, val)
+        {
+            output += '<li>';
+            output += '<h2>' + val.name + '</h2>';
+            output += '<img src="images/' + val.shortname + '_tn.jpg" alt' + val.name + '"/>';
+            output += '<p>' + val.bio + '</p>';
+            output += '</li>';
+        });
+        output += '</ul>';
+        $("#update").html(output);
+    }); //GET JSON
+});
+
